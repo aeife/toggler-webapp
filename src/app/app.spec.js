033dba08e4f9e4c6ac0b2b21b257bbe82599e826
aeife <andre.eife@gmail.com>
2015-05-01 15:31:32 +0200

    initial boilerplate setup

---
+++ src/app/app.spec.js
@@ -0,0 +1,11 @@
+'use strict';
+
+describe('toggler', function() {
+    beforeEach(function () {
+        module('toggler');
+    });
+
+    it('dummy test', function() {
+        expect(true).toBe(true);
+    });
+});
